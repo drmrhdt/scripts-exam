079b30548478f06e25a84cbc2de464125ad45178
drmrhdt <drmrhdt@gmail.com>
2020-01-18 06:12:45 +0300

    Add base

---
+++ build/precache-manifest.0fde90e7046b7b4c1a1f8e48f841f96e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9a84c2ad9f0bacefb059816aec7b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baca987adb1edcb60316",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e99afc4eaef55b43aea8",
+    "url": "/static/css/main.1ce3db26.chunk.css"
+  },
+  {
+    "revision": "baca987adb1edcb60316",
+    "url": "/static/js/2.d348696b.chunk.js"
+  },
+  {
+    "revision": "e99afc4eaef55b43aea8",
+    "url": "/static/js/main.5c8ea603.chunk.js"
+  },
+  {
+    "revision": "e65c27e9baa56c98f725",
+    "url": "/static/js/runtime-main.d3e71db8.js"
+  }
+]);
---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB-0vEI4-mTx-BA1EP71tx2w6kbPfsVncg",
+  authDomain: "scripts-exam-5b366.firebaseapp.com",
+  databaseURL: "https://scripts-exam-5b366.firebaseio.com",
+  projectId: "scripts-exam-5b366",
+  storageBucket: "scripts-exam-5b366.appspot.com",
+  messagingSenderId: "395674620826",
+  appId: "1:395674620826:web:5b7820b8d7226e847d6023"
+};
+
+firebase.initializeApp(firebaseConfig);
